6a1b85c4ec2ea765933c0b9b424f0cbe5a0da395
Amy F <snowstep231@gmail.com>
2025-07-10 22:01:04 -0400

    added event listener to add products to cart, created object for product info stored in cart. tested. made sure event listener was below function. confirmed product info logged in console.

--- product.js
+++ product.js
@@ -9,3 +9,29 @@ if (productId === "1") {
     document.getElementById("product-descripton").textContent = "Available in sizes S, M, L, XL, 2X.  One of a kind, hand-dyed cotton shirt.";
     document.getElementById("product-price").textContent = "$25";
 }
+
+if (productId === "2") {
+    document.getElementById("product-name").textContent = "Macramé Potholder";
+    document.getElementById("product-descripton").textContent = "These sturdy macramé potholders keep your plants suspended in style!  The pot is included.";
+    document.getElementById("product-price").textContent = "$20";
+}
+
+function handleAddToCart() {
+    console.log("Button clicked!");
+    
+    let productName = document.getElementById("product-name").textContent;
+    let productPrice = document.getElementById("product-price").textContent = "$25";
+    let productColor = document.getElementById("color").value;
+    let productSize = document.getElementById("size").value;
+
+    const cartItem = {
+        name: productName,
+        price: productPrice,
+        color: productColor,
+        size: productSize
+    };
+
+    console.log(cartItem);
+}
+
+document.getElementById("addToCart").addEventListener("click", handleAddToCart);
