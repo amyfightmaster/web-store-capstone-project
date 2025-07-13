ae2c65af572be4e1b00c2ac4aef8e1dd17a44516
Amy F <snowstep231@gmail.com>
2025-07-13 16:03:20 -0400

    added further CSS styling to products.html--tweaking layout and styling add to cart button.  Also added additional product cards to product.js and product.html.

--- product.js
+++ product.js
@@ -11,9 +11,39 @@ if (productId === "1") {
 }
 
 if (productId === "2") {
-    document.getElementById("product-name").textContent = "Macramé Potholder";
-    document.getElementById("product-descripton").textContent = "These sturdy macramé potholders keep your plants suspended in style!  The pot is included.";
+    document.getElementById("product-name").textContent = "Macramé Pot-hangers";
+    document.getElementById("product-descripton").textContent = "These sturdy macramé pot-hangers keep your plants suspended in style!  The pot is included.";
     document.getElementById("product-price").textContent = "$20";
+}
+
+if (productId === "3") {
+    document.getElementById("product-name").textContent = "Macramé Keychains";
+    document.getElementById("product-descripton").textContent = "his keychain is perfect for securing your keys, and its vibrant colors draw the eye well (and also help you remember where you put your keys).";
+    document.getElementById("product-price").textContent = "$20";
+}
+
+if (productId === "4") {
+    document.getElementById("product-name").textContent = "Paracord Bracelets";
+    document.getElementById("product-descripton").textContent = "This stylish bracelet is sure to make a rugged statement.  Available in three sizes.";
+    document.getElementById("product-price").textContent = "$15";
+}
+
+if (productId === "5") {
+    document.getElementById("product-name").textContent = "Dreamcatchers";
+    document.getElementById("product-descripton").textContent = "These dreamcatchers are made using cotton yarn and other materials.  Available in 'baby' sizes, small, medium.";
+    document.getElementById("product-price").textContent = "$15";
+}
+
+if (productId === "6") {
+    document.getElementById("product-name").textContent = "Potholders";
+    document.getElementById("product-descripton").textContent = "These sturdy potholders are made from 100% cotton, meaning that they are both fine and functional.";
+    document.getElementById("product-price").textContent = "$15";
+}
+
+if (productId === "7") {
+    document.getElementById("product-name").textContent = "Suncatchers";
+    document.getElementById("product-descripton").textContent = "These beaded suncatchers bring light and cheer, and the crystal upon the end reflects the sun's light.";
+    document.getElementById("product-price").textContent = "$15";
 }
 
 function handleAddToCart() {
