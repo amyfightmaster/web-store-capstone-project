9612cc4130b7cdf6e0ab39af33dfb74f552e4d32
Amy F <snowstep231@gmail.com>
2025-07-16 13:09:41 -0400

    items now showing up on cart page. Attempted to add clear cart button. click not defined error. in proper event listener, updated (click, function() => {etc., etc.}). Clear from cart button functional.

--- cart.js
+++ cart.js
@@ -2,3 +2,24 @@ console.log("cart.js is loaded!  Yay!")
 
 let cartItems = JSON.parse(localStorage.getItem("cart")) || [];
 console.log("Cart items from localStorage:", cartItems);
+
+let cartContainer = document.getElementById("cart-items");
+
+cartItems.forEach(item => {
+    let itemDiv = document.createElement("div");
+    itemDiv.classList.add("cart-item");
+
+    itemDiv.innerHTML = `
+        <h3>${item.name}</h3>
+        <p>Price: ${item.price}</p>
+        ${item.size ? `<p>Size: ${item.size}</p>` : ""}
+        ${item.color ? `<p>Color: ${item.color}</p>` : ""}
+    `;
+
+    cartContainer.appendChild(itemDiv);
+});
+
+document.getElementById("clear-cart").addEventListener("click", () => { //event listener to remove stuff from cart//
+    localStorage.removeItem("cart");
+    location.reload; //this reloads page, to show cleared cart//
+})
